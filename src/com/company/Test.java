6b93e368cb92d39c7057384edd0eb9a1db641452
Ulukbek14 <ulukbekkylycbekov@gmail.com>
2021-04-22 19:44:09 +0600

    Initial commit

---
+++ src/com/company/Test.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Test {
+    String str = "8:170.0;10:500.0;30:800.0;50:3495.0->1.0:-1.0";{
+        System.out.println(Arrays.toString(str.split(";")));
+
+    }
+
+
+}
